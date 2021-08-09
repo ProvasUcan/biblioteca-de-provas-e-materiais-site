605d2fe3bb081051fe9700976be04787451415c1
EufranioDiogo <eufraniodiogo5@gmail.com>
2021-08-09 01:17:16 +0100

    Fazendo o menu para download

---
+++ JS/index.js
@@ -0,0 +1,176 @@
+const app = new Vue({
+    el: "#menu",
+    data: {
+        engInformatica: {
+            '1º Ano': {
+                '1º Semestre': {
+                    'Analise Matemática': {
+                        'Frequências': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                        'Exames': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                        'Recursos': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                        'Exercícios': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                    },
+                    'Fundamentos de Programação 1': {
+                        'Frequências': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                        'Exames': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                        'Recursos': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                        'Exercícios': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                    },
+                    'Fisica Moderna': {
+                        'Frequências': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                        'Exames': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                        'Recursos': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                        'Exercícios': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                    },
+                    'Algebra': {
+                        'Frequências': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                        'Exames': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                        'Recursos': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                        'Exercícios': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                    }
+                }
+            },
+            '2º Ano': {
+                '1º Semestre': {
+                    'Analise Matemática': {
+                        'Frequências': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                        'Exames': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                        'Recursos': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                        'Exercícios': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                    },
+                    'Fundamentos de Programação 1': {
+                        'Frequências': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                        'Exames': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                        'Recursos': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                        'Exercícios': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                    },
+                    'Fisica Moderna': {
+                        'Frequências': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                        'Exames': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                        'Recursos': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                        'Exercícios': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                    },
+                    'Algebra': {
+                        'Frequências': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                        'Exames': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                        'Recursos': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                        'Exercícios': {
+                            link: "#", 
+                            isReadyToDownload: false
+                        },
+                    }
+                }
+            }
+        }
+    },
+})
+
+//app.initialize();
+// 
+// 
+const openMenu = document.querySelector(".open-menu");
+const closeMenu = document.querySelector(".close-menu");
+const menu = document.querySelector('.main-menu');
+
+openMenu.onClick = () => {
+    menu.style.opacity = 1;
+    closeMenu.style.opacity = 1;
+}
+
+closeMenu.onClick = (element) => {
+    menu.style.opacity = 0;
+    closeMenu.style.opacity = 0;
+}
