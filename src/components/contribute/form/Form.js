fabffa6ecf2d117d8b11d152a2df71b130d18de7
EufranioDiogo <eufraniodiogo5@gmail.com>
2021-10-05 12:29:08 +0100

    chore: start building a better interface

--- src/components/contribute/form/Form.js
+++ src/components/contribute/form/Form.js
@@ -5,6 +5,7 @@ const Form = ({ id, handleDelete, allCoursesStructure }) => {
   const [avaibleYears, setAvaibleYears] = useState([]);
   const [avaibleSemestres, setAvaibleSemestres] = useState([]);
   const [avaibleSubjects, setAvaibleSubjects] = useState([]);
+  const [filesToSubmitList, setFilesToSubmitList] = useState([]);
 
   const [courseSelected, setCourseSelected] = useState('');
   const [yearSelected, setYearSelected] = useState('');
@@ -54,26 +55,45 @@ const Form = ({ id, handleDelete, allCoursesStructure }) => {
     e.preventDefault();
     var form = new FormData();
 
-    const destFolder = `${document.getElementById('course').value},${document.getElementById('year').value},${document.getElementById('semestre').value},${document.getElementById('subject').value},${document.getElementById('documentType').value}`;
+    const destFolder = `${document.getElementById('course').value},${document.getElementById('year').value},${document.getElementById('semestre').value}, ${document.getElementById('subject').value}, ${document.getElementById('documentType').value}`;
     const userEmail = document.getElementById('userEmail').value
     const files = document.getElementById('files').files;
 
     form.append('destFolder', destFolder);
     form.append('userEmail', userEmail);
-    form.append('files', files);
 
-    console.log(form)
+    for (let actualFileIndex = 0; actualFileIndex < files.length; actualFileIndex++) {
+      form.append('files', files[actualFileIndex]);
+    }
 
     fetch("http://localhost:3000/upload/submission", {
       method: "POST",
       body: form
     })
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        handleDelete(id)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  const deleteIndividualFile = (index) => {
+    const auxFileList = filesToSubmitList.filter((file, i) => i !== index);
+    setFilesToSubmitList(auxFileList);
+  }
+
+  const addFiles = () => {
+    const files = document.getElementById('files').files;
+    const auxFilesList = [];
+
+    for (let actualFileIndex = 0; actualFileIndex < files.length; actualFileIndex++) {
+      auxFilesList.push(files[actualFileIndex]);
+    }
+
+    setFilesToSubmitList(filesToSubmitList.concat(auxFilesList));
   }
 
   useEffect(() => {
@@ -81,9 +101,13 @@ const Form = ({ id, handleDelete, allCoursesStructure }) => {
   }, [])
 
   return (
-    <form id={id} enctype="multipart/form-data" className="form-container" method="POST" onSubmit={(e) => {
+    <form id={id} encType="multipart/form-data" className="form-container" method="POST" onSubmit={(e) => {
       submitForm(e, id);
     }}>
+      <button className="delete-button" onClick={() => {
+        handleDelete(id);
+      }}>Deletar</button>
+
       <h1 className="subject-form-name">{subjectSelected}</h1>
       <div className="form-row-line">
         <select name="course" id="course" className="search-container--element" >
@@ -126,13 +150,27 @@ const Form = ({ id, handleDelete, allCoursesStructure }) => {
           <option value="Recursos">Recursos</option>
         </select>
       </div>
-      <input type="email" name="userEmail" id="userEmail" placeholder="example@gmail.com"/>
-      <button className="delete-button" onClick={() => {
-        handleDelete(id);
-      }}>Deletar</button>
+      <input type="email" name="userEmail" id="userEmail" placeholder="example@gmail.com" className="input-field-text" />
+
 
       <div className="photo-container">
-        <input type="file" name="files" id="files" multiple="true" />
+        {
+          filesToSubmitList.map((file, index) => (
+            <div className="individual-file-container">
+              <img src={URL.createObjectURL(file)} alt="" className="file-preview-img" />
+
+              <button className="individual-file-container-delete-button" onClick={() => {
+                deleteIndividualFile(index);
+              }}>Deletar</button>
+            </div>
+
+          ))
+        }
+
+
+        <div className="add-more-content">
+          <input type="file" name="files" id="files" onChange={addFiles} multiple="true" />
+        </div>
       </div>
 
       <button type="submit">Enviar</button>
