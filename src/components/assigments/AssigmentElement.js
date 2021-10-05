507b21effaf96453a5e5be93f4b8d6ff8a45896a
EufranioDiogo <eufraniodiogo5@gmail.com>
2021-10-06 00:18:06 +0100

    chore: stable version with bug in search engine in change of selections

--- src/components/assigments/AssigmentElement.js
+++ src/components/assigments/AssigmentElement.js
@@ -12,18 +12,11 @@ const AssigmentElement = ({ assigment, id }) => {
             <img src="./img/fluent_eye-show-24-filled.svg" alt="View assigment" className="hover-container-menu--item-img" />
           </li>
 
-          <a href={assigment} className="hover-container-menu--item" download>
+          <a href={assigment} className="hover-container-menu--item" download={assigment}>
             <img src="./img/akar-icons_download.svg" alt="Download assigment" className="hover-container-menu--item-img" />
           </a>
         </ul>
       </div>
-
-      {isPeding &&
-        <div className="loading-container">
-          <img src="./img/eos-icons_loading.svg" alt="Loading Item" className="loading-element" />
-        </div>
-      }
-
     </div>
   );
 }
