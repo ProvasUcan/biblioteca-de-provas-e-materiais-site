b3da837ba07111c840cb587ee6f8e71889382988
EufranioDiogo <eufraniodiogo5@gmail.com>
2021-11-11 09:51:13 +0100

    feat: new services form done

--- src/services/remote/user/user.js
+++ src/services/remote/user/user.js
@@ -1,14 +1,52 @@
 import { apiBaseUrl } from "../../../config/apiConfig"
 
 export const getActualUserData = async () => {
-  await fetch(`${apiBaseUrl}/user`, {
-    method: 'GET'
+  const res = await fetch(`${apiBaseUrl}/user`, {
+    method: 'GET',
+    headers: {
+      'auth-token': localStorage.getItem('auth-token-biblioteca-de-provas')
+    }
   })
-  .then(res => res.json())
-  .then(data => {
-    return data
+  const data = await res.json()
+
+  return data;
+}
+
+export const getUserById = async (userId) => {
+  const res = await fetch(`${apiBaseUrl}/user/${userId}`, {
+    method: 'GET',
+    headers: {
+      'auth-token': localStorage.getItem('auth-token-biblioteca-de-provas')
+    }
   })
-  .catch(err => {
-    console.log(err)
+  const data = await res.json()
+
+  return data.data;
+}
+
+export const getContributors = async (userId) => {
+  const res = await fetch(`${apiBaseUrl}/user/contributors`, {
+    method: 'GET',
+    headers: {
+      'auth-token': localStorage.getItem('auth-token-biblioteca-de-provas')
+    }
   })
+  const data = await res.json()
+
+  return data.data;
+}
+
+export const updateUser = async (userId, body) => {
+  console.log(userId, body)
+  const res = await fetch(`${apiBaseUrl}/user/${userId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'auth-token': localStorage.getItem('auth-token-biblioteca-de-provas')
+    },
+    body: JSON.stringify(body)
+  })
+  const data = await res.json()
+
+  return data;
 }
