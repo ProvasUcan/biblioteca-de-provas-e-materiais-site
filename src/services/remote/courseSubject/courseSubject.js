ef206d15fe2993d76e8bd5d6653bf0341627b2b2
EufranioDiogo <eufraniodiogo5@gmail.com>
2021-11-11 09:45:50 +0100

    feat: courseSubject association remote updated to new requests

---
+++ src/services/remote/courseSubject/courseSubject.js
@@ -0,0 +1,55 @@
+import { apiBaseUrl } from "../../../config/apiConfig";
+
+export const createCourseSubject = async (body) => {
+  const res = await fetch(`${apiBaseUrl}/association`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'auth-token': localStorage.getItem('auth-token-biblioteca-de-provas')
+    },
+    body: JSON.stringify(body)
+  })
+  const data = await res.json()
+
+  return data;
+}
+
+export const getCoursesSubjects = async () => {
+  const res = await fetch(`${apiBaseUrl}/association`, {
+    method: 'GET',
+    headers: {
+      'auth-token': localStorage.getItem('auth-token-biblioteca-de-provas')
+    }
+  })
+  const data = await res.json()
+  console.log(data)
+
+  return data.courses;
+}
+
+export const deleteCourseSubject = async (courseId) => {
+  const res = await fetch(`${apiBaseUrl}/association/${courseId}`, {
+    method: 'DELETE',
+    headers: {
+      'auth-token': localStorage.getItem('auth-token-biblioteca-de-provas')
+    }
+  })
+  const data = await res.json()
+
+  return data.data;
+}
+
+export const updateCourseSubject = async (courseId, body) => {
+  const res = await fetch(`${apiBaseUrl}/association/${courseId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'auth-token': localStorage.getItem('auth-token-biblioteca-de-provas')
+    },
+    body: JSON.stringify(body)
+  })
+  const data = await res.json()
+  console.log(data)
+
+  return data.data;
+}
