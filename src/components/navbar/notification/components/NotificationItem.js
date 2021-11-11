3ba5bcd13191cc27f8b7d496734fb29eaa69552f
EufranioDiogo <eufraniodiogo5@gmail.com>
2021-11-11 09:28:40 +0100

    feat: notification item developed

---
+++ src/components/navbar/notification/components/NotificationItem.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+function NotificationItem({
+  id,
+  notificationType,
+  referenceId,
+  createdAt,
+  isReaded,
+  handleShowNotificationContainer
+}) {
+  const [subject, setSubject] = useState('')
+
+  return (
+    <Link 
+    to={`/biblioteca-de-provas-e-materiais-site/notification/${id}`} 
+    className={(isReaded ? 'readed-notification' : 'unreaded-notification') + ' notification-item'}
+    onClick={() => {
+      handleShowNotificationContainer(false)
+    }}
+    >
+      <h3 className="notification-type">
+        {
+          notificationType.includes('APPROVED') ? 'Submissão aceite!' : 'Submissão recusada!'
+        }
+      </h3>
+
+      <p className="notification-date">{createdAt}</p>
+    </Link>
+  )
+}
+
+export default NotificationItem
