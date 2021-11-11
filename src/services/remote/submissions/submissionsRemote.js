e6a6dc75f79f713760c3556384f1d0a5a94d6c58
EufranioDiogo <eufraniodiogo5@gmail.com>
2021-11-11 09:48:41 +0100

    feat: submissions remote updated to new requests

---
+++ src/services/remote/submissions/submissionsRemote.js
@@ -0,0 +1,101 @@
+import { apiBaseUrl } from "../../../config/apiConfig"
+
+export const getApprovedSubmissions = async () => {
+  const response = await fetch(`${apiBaseUrl}/submission/approve`,
+  {
+    method: 'GET',
+    headers: {
+      'auth-token': localStorage.getItem('auth-token-biblioteca-de-provas')
+    }
+  })
+  const data  = await response.json();
+  return data.submissions;
+}
+
+
+export const getNotApprovedSubmissions = async () => {
+  const response = await fetch(`${apiBaseUrl}/submission/unapprove`,
+  {
+    method: 'GET',
+    headers: {
+      'auth-token': localStorage.getItem('auth-token-biblioteca-de-provas')
+    }
+  })
+  const data  = await response.json();
+  return data.submissions;
+}
+
+export const getRejectedSubmissions = async () => {
+  const response = await fetch(`${apiBaseUrl}/submission/reject`,
+  {
+    method: 'GET',
+    headers: {
+      'auth-token': localStorage.getItem('auth-token-biblioteca-de-provas')
+    }
+  })
+  const data  = await response.json();
+
+  return data.submissions;
+}
+
+export const getSpecificSubmission = async (submissionId) => {
+  const response = await fetch(`${apiBaseUrl}/submission/${submissionId}`,
+  {
+    method: 'GET',
+    headers: {
+      'auth-token': localStorage.getItem('auth-token-biblioteca-de-provas')
+    }
+  })
+  const data  = await response.json();
+
+  return data;
+} 
+
+export const approveSubmission = async (submissionId) => {
+  const response = await fetch(`${apiBaseUrl}/submission/approve/${submissionId}`,
+  {
+    method: 'PUT',
+    headers: {
+      'auth-token': localStorage.getItem('auth-token-biblioteca-de-provas')
+    }
+  })
+  const data  = await response.json();
+  return data;
+}
+
+export const desapproveSubmission = async (submissionId) => {
+  const response = await fetch(`${apiBaseUrl}/submission/unapprove/${submissionId}`,
+  {
+    method: 'PUT',
+    headers: {
+      'auth-token': localStorage.getItem('auth-token-biblioteca-de-provas')
+    }
+  })
+  const data  = await response.json();
+  return data;
+}
+
+
+export const rejectSubmission = async (submissionId) => {
+  const response = await fetch(`${apiBaseUrl}/submission/reject/${submissionId}`,
+  {
+    method: 'PUT',
+    headers: {
+      'auth-token': localStorage.getItem('auth-token-biblioteca-de-provas')
+    }
+  })
+  const data  = await response.json();
+  return data.status;
+}
+
+export const deleteSubmission = async (submissionId) => {
+  const response = await fetch(`${apiBaseUrl}/submission/${submissionId}`,
+  {
+    method: 'DELETE',
+    headers: {
+      'auth-token': localStorage.getItem('auth-token-biblioteca-de-provas')
+    }
+  })
+  const data  = await response.json();
+  return data.status;
+}
