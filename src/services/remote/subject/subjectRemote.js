39b820dc7e7704a1f9c75d120810b11af4a5c2c0
EufranioDiogo <eufraniodiogo5@gmail.com>
2021-11-11 09:47:24 +0100

    feat: subject remote updated to new requests

---
+++ src/services/remote/subject/subjectRemote.js
@@ -0,0 +1,61 @@
+import { apiBaseUrl } from "../../../config/apiConfig";
+
+export const getAllSubjectsStructure = async function () {
+  let auxAllSubjectsStructure = await fetch(`${apiBaseUrl}/subject/all`);
+  auxAllSubjectsStructure = await auxAllSubjectsStructure.json();
+
+  return auxAllSubjectsStructure.subjects;
+}
+
+export const createSubject = async (body) => {
+  const res = await fetch(`${apiBaseUrl}/subject`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'auth-token': localStorage.getItem('auth-token-biblioteca-de-provas')
+    },
+    body: JSON.stringify(body)
+  })
+  const data = await res.json()
+
+  return data;
+}
+
+export const getSubjects = async () => {
+  const res = await fetch(`${apiBaseUrl}/subject`, {
+    method: 'GET',
+    headers: {
+      'auth-token': localStorage.getItem('auth-token-biblioteca-de-provas')
+    }
+  })
+  const data = await res.json()
+
+  return data.subjects;
+}
+
+export const deleteSubject = async (subjectId) => {
+  const res = await fetch(`${apiBaseUrl}/subject/${subjectId}`, {
+    method: 'DELETE',
+    headers: {
+      'auth-token': localStorage.getItem('auth-token-biblioteca-de-provas')
+    }
+  })
+  const data = await res.json()
+
+  return data.data;
+}
+
+export const updateSubject = async (subjectId, body) => {
+  const res = await fetch(`${apiBaseUrl}/subject/${subjectId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'auth-token': localStorage.getItem('auth-token-biblioteca-de-provas')
+    },
+    body: JSON.stringify(body)
+  })
+  const data = await res.json()
+  console.log(data)
+
+  return data.data;
+}
