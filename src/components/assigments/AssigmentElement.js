ba619e26e4a3f20d08b79e49511fad8e0b740b79
EufranioDiogo <eufraniodiogo5@gmail.com>
2021-11-14 00:43:04 +0100

    fix: download image with extension

--- src/components/assigments/AssigmentElement.js
+++ src/components/assigments/AssigmentElement.js
@@ -9,18 +9,21 @@ const AssigmentElement = ({ assigment, id, previewAssigment, downloadAssigment }
     <div className="assigment-item" onClick={() => {
       previewAssigment(id)
     }}>
-      <img id={'img-' + id} src={apiBaseUrl + '/' + assigment} alt="" className="assigment-item-preview" />
+      <img id={'img-' + id} src={assigment} alt="" className="assigment-item-preview" />
 
       <div className="assigment-item-hover-container">
         <ul className="hover-container-menu">
           <li className="hover-container-menu--item">
-            <img src="./img/fluent_eye-show-24-filled.svg" alt="View assigment" className="hover-container-menu--item-img" onClick={() => {
+            <img src="./img/fluent_eye-show-24-filled.svg" alt="View assigment" className="hover-container-menu--item-img" onClick={(e) => {
+              e.stopPropagation();
               previewAssigment(id)
             }} />
           </li>
 
-          <li onClick={() => {
-            downloadAssigment(apiBaseUrl + '/' + assigment, searchContext.name)
+          <li onClick={(e) => {
+            e.stopPropagation();
+            console.log('Assigment ', assigment)
+            downloadAssigment(assigment, searchContext.name)
           }} className="hover-container-menu--item">
             <img src="./img/akar-icons_download.svg" alt="Download assigment" className="hover-container-menu--item-img" />
           </li>
