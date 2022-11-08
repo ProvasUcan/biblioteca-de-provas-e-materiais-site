faaa7dfcb99ecf302dfb62566cd95547bc9c9318
EufranioDiogo <eufraniodiogo5@gmail.com>
2022-11-08 15:31:18 +0100

    fix: changing the url to production url

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,2 +1,2 @@
-//export const apiBaseUrl = 'https://biblioteca-de-provas-ucan-backend.onrender.com'
-export const apiBaseUrl = 'http://localhost:3000'
+export const apiBaseUrl = 'https://biblioteca-de-provas-ucan-backend.onrender.com'
+//export const apiBaseUrl = 'http://localhost:3000'
